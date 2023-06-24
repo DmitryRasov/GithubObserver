7e8a5ff5b6b7f6ecedc394656678eb62abaa8ec2
DmitryRasov <letucepray@yandex.ru>
2023-06-24 16:51:40 +0300

    added results component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <email-result :emails="emails"/>
+    <form @click.prevent>
+      <input
+          type="text"
+          v-model="query"
+      >
+      <button @click="getRepos(query)">Find</button>
+    </form>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EmailResult from "@/components/EmailResult.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    EmailResult
+
+  },
+  data() {
+    return {
+      title: 'hello there',
+      query: "",
+      repository: '',
+      emails: new Set(),
+    }
+  },
+  methods: {
+    async getRepos(userName){
+      const result = await fetch(`https://api.github.com/users/${userName}/repos`).then(res => res.json())
+      for (let i = 0; i < result.length; i++) {
+        if (result[i]?.fork === false) {
+          this.repository = result[i]?.name
+          this.getCommits(userName, this.repository)
+        }
+      }
+    },
+    async getCommits(userName, repo){
+      const commits = await fetch(`https://api.github.com/repos/${userName}/${repo}/commits`).then(res => res.json())
+      console.log(commits[0]?.commit?.author?.email)
+      if (!commits[0]?.commit?.author?.email?.includes('noreply')) {
+        this.emails.add(commits[0]?.commit?.author?.email)
+      }
+    }
   }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/EmailResult.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-for="email in emails" :key="email">
+    <h1>{{ email }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmailResult',
+  props: {
+    emails: {
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
